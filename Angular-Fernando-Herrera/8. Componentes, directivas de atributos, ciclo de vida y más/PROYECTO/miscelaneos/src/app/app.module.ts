8fe4c2b6b9c9e1de03b6927fe5089c0ae09cf794
fernandomiras1 <ernando.miras.pc@gmail.com>
2019-03-07 23:20:21 -0300

    Angular-Fernando-Herrera

---
+++ Angular-Fernando-Herrera/8. Componentes, directivas de atributos, ciclo de vida y más/PROYECTO/miscelaneos/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgStyleComponent,
+    CssComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-Fernando-Herrera/8. Componentes, directivas de atributos, ciclo de vida y más/PROYECTO/miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  templateUrl: './ng-style.component.html',
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano = 20;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
