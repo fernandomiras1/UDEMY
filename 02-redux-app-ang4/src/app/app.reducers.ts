85a7af78cbec6767df4bde3f4cd59c78b3aff0f1
fernandomiras1 <ernando.miras.pc@gmail.com>
2019-03-07 22:41:16 -0300

    Eliminacion de todo

--- 02-redux-app-ang4/src/app/app.reducers.ts
+++
@@ -1,7 +0,0 @@
-
-
-
-export interface AppState {
-    contador: number;
-}
-
--- 03-todoapp/src/app/app.reducers.ts
+++
@@ -1,8 +0,0 @@
-import { Todo } from './todo/model/todo.model';
-
-// Definimos el estado de mi aplicacion ( forma global que va a manejar mi store )
-
-// Definimos los datos q va a manejar mi reducer
-export interface AppState {
-    todos: Todo[];
-}
