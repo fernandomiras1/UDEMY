81e6711d980ccfa57a77930d50c84daa16ba97aa
fernandomiras1 <ernando.miras.pc@gmail.com>
2019-04-15 12:26:01 -0300

    Finalizaccion Seccion 6 - REDUX

--- Angular-REDUX-NGRX-Fernando-Herrera/PROYECTO/ingresoEgresoApp/src/app/app.module.ts
+++ Angular-REDUX-NGRX-Fernando-Herrera/PROYECTO/ingresoEgresoApp/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
