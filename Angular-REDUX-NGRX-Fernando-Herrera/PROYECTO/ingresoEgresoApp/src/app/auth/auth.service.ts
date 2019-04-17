26eafab1a5c8362d4ae30ab26e64b7add829f3c2
Fernando Miras <fernando.miras.pc@gmail.com>
2019-04-17 20:50:52 -0300

    Fin de la session 7 - REDUX

--- Angular-REDUX-NGRX-Fernando-Herrera/PROYECTO/ingresoEgresoApp/src/app/auth/auth.service.ts
+++ Angular-REDUX-NGRX-Fernando-Herrera/PROYECTO/ingresoEgresoApp/src/app/auth/auth.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
+
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
+
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { User } from './user.model';
 
 // No hace falta declararlo en mi app.moduletsts porque esta injectado de esta forma
 @Injectable({
@@ -8,18 +14,42 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  constructor( private afAuth: AngularFireAuth, private router: Router ) { }
+  constructor( private afAuth: AngularFireAuth, 
+              private router: Router,
+              private afDB: AngularFirestore ) { }
+
+  // con FireBase podemos obtener la info del usuario que se encuentra logeado en ese momento. 
+  // Si es null es porque no esta logeado
+  initAuthListener() {
+    this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
+      console.log(fbUser);
+    });
+  }
 
   crearUsuario(nombre: string, email: string, password: string): void {
 
     this.afAuth.auth
       .createUserWithEmailAndPassword( email, password).then( resu => {
           console.log('resu', resu);
-          this.router.navigate(['/']);
+
+          const user: User = {
+            nombre: nombre,
+            uid: resu.user.uid,
+            email: resu.user.email
+          }
+          // Creo el Documento en FireBase
+          // el user.id es lo que va a mostrar el la collecion
+          // el nombre: usuario es lo que va a guardar en la colleccion. 
+          this.afDB.doc(`${ user.uid }/usuario`)
+            .set( user )
+            // Si todo lo hace correctamente, navego al dasboard
+            .then( ()=> {
+              this.router.navigate(['/']);
+            });
+
       })
       .catch( error => { 
         console.error(error);
-        
       });
   }
 
@@ -32,9 +62,30 @@ export class AuthService {
       })
       .catch( error => { 
         console.error(error);
-        
       });
   }
 
+  // cerrar session
+  logout() {
+    this.router.navigate( ['/login'] );
+    this.afAuth.auth.signOut();
+  }
+
+
+  // Verficamos si esta logeado
+  // este metodo devuelve un observable
+  isAuth() {
+   return this.afAuth.authState.pipe(
+     map( fbUser => {
+       
+      if ( fbUser == null ) {
+        this.router.navigate(['/login']);
+      }
+
+      return fbUser != null
+     })
+     );
+   
+  }
 
 }
