27a23b1cb6a66b74808880d906ee078fbf01e54b
Fernando Miras <fernando.miras.pc@gmail.com>
2019-05-05 00:10:36 -0300

    Curso de JavaScript 01-app-basic

---
+++ JavaScript/01-app-basic/objetos.js
@@ -0,0 +1,15 @@
+// Objetos en JavaScript
+
+const persona = {
+    nombre: 'Fernadno',
+    apellido: 'Miras',
+    profesion: 'Desarrollador Web',
+    email: 'fernando.miras.pc@gmail.com'
+}
+
+
+const autos = [
+    { modelo: 'Mustag', motor: '6.0'},
+    { modelo: 'Camaro', motor: '7 .0'}
+]
+console.log(autos);
