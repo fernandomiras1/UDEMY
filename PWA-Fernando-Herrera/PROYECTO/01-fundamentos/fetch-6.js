e8b5f78b42d040783e1cf1278fb9ab1e2816b71a
Fernando Miras <fernando.miras.pc@gmail.com>
2019-05-18 22:00:57 -0300

    FIN Reforzamiento Promesas, Fetch API y HttpServer

--- PWA-Fernando-Herrera/PROYECTO/01-fundamentos/fetch-6.js
+++ PWA-Fernando-Herrera/PROYECTO/01-fundamentos/fetch-6.js
@@ -1,11 +1,14 @@
 
 
-
+// Obtenemos y leemos  todo el archivo de html  
 fetch('no-encontrado.html')
     .then( resp => resp.text() )
     .then( html => {
-
+        // te devuelve todo el texto (contenido) q tiene esa pag.
+        console.log(html);
         let body = document.querySelector('body');
+
+        // Reemplazamos el el body de mi pagina por el no-encontrado.html
         body.innerHTML = html;
 
 
