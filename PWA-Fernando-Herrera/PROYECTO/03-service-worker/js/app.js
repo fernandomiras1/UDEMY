ef2c694a6da095309ca2851e4e6e000616cd081d
Fernando Miras <fernando.miras.pc@gmail.com>
2019-05-25 14:11:40 -0300

    FIN 03-service-worker

--- PWA-Fernando-Herrera/PROYECTO/03-service-worker/js/app.js
+++ PWA-Fernando-Herrera/PROYECTO/03-service-worker/js/app.js
@@ -2,5 +2,18 @@
 
 // Detectar si podemos usar Service Workers
 if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('sw.js')
+        .then(sw => {
+            // SYNC: mandamos datos al SRV cuando no hay internet
+            // setTimeout(() => {
+            //     sw.sync.register('post-gatitos');
+            //     console.log('Se enviarion fotos de gatitos al server');
+            // }, 3000);
+
+            // PUSH: Mandamos un Push desde la app
+            Notification.requestPermission().then( resu => {
+                console.log(resu);
+                sw.showNotification('Hola Mundo !');
+            });
+        });
 }
