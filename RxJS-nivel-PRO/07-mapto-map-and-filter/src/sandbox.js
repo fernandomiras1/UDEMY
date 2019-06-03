d8c7f654d07687947ec28de60825f76ee1d6b144
Fernando Miras <fernando.miras.pc@gmail.com>
2019-06-03 11:28:54 -0300

    RxJS-nivel-PRO/07-mapto-map-and-filter

--- RxJS-nivel-PRO/07-mapto-map-and-filter/src/sandbox.js
+++ RxJS-nivel-PRO/07-mapto-map-and-filter/src/sandbox.js
@@ -1,7 +1,7 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
 // mapTo: Es muy simple, devuelve la salida siempre en el mismo valor
-import { mapTo, map, filter, tap, first, take, takeWhile } from 'rxjs/operators';
+import { mapTo, map, filter, tap, first, take, takeWhile, skip } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -9,9 +9,11 @@ export default () => {
     const click$ = fromEvent(grid, 'click').pipe(
         // en este caso devovlemos en el clikc, el string 'click'
         // mapTo('click'),
+
         // tap:  leer informacion del string de datos y generar acciones
         tap( val => console.log('before', val)),
         map(val => [
+            // Math.floor ( me devuele la parte entera sin redondear ).
             Math.floor(val.offsetX / 50),
             Math.floor(val.offsetY / 50 )
         ]),
@@ -28,7 +30,13 @@ export default () => {
         //take(4),
         
         // takeWhile: podemos hacer que se ejecute la accion mietras se cumpla x condicion
-        takeWhile( ([col, row]) => col > 3)
+        // Mientras que la columna sea mayor a 3, voy a estar emitiendo eventos.
+        // takeWhile( ([col, row]) => col > 3),
+        
+        tap(val => console.log(`cell: [ ${val}]`)),
+        
+        // Omitir los primeros 5 eventos. despues del 5 ya emite los demas eventos.
+        skip(5)
 
     );
     const subscription = click$.subscribe( event => displayLog(event));
