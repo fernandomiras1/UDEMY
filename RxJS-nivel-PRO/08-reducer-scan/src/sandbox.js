f105dcc0c13a0f5bc382709db9c1e944784a47dc
Fernando Miras <fernando.miras.pc@gmail.com>
2019-06-03 12:00:59 -0300

    FIN RxJS-nivel-PRO/08-reducer-scan

--- RxJS-nivel-PRO/08-reducer-scan/src/sandbox.js
+++ RxJS-nivel-PRO/08-reducer-scan/src/sandbox.js
@@ -1,46 +1,39 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-// mapTo: Es muy simple, devuelve la salida siempre en el mismo valor
-import { mapTo, map, filter, tap, first, take, takeWhile, skip } from 'rxjs/operators';
+import { map, takeWhile, tap, reduce, scan } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
-        // en este caso devovlemos en el clikc, el string 'click'
-        // mapTo('click'),
-
-        // tap:  leer informacion del string de datos y generar acciones
-        tap( val => console.log('before', val)),
         map(val => [
             // Math.floor ( me devuele la parte entera sin redondear ).
             Math.floor(val.offsetX / 50),
             Math.floor(val.offsetY / 50 )
         ]),
-        // filtro los pares
-        // filter( val => ( val[0] + val[1] % 2 != 0)),
-        // tap( val => console.log('after', val)),
+        // Finalizo los eventos, cuanto toco la columna 0
+        takeWhile(([col, row]) => col != 0),
+        tap( val => console.log(`cell: [${val}]`)),
+        // reduce: aplica una misma funcion a cada evento que llega por el stirng de forma secuencial.
+        // scan: se parece al reduce, apilica un acumulador al flujo de eventos, 
+        // pero la diferencia es que cada vez que reciebe un evetno, scan emite un evento con el valor acumulado 
         
-        // first: te permite emitir una sola accion. 
-        // a demas le podemos poner logia. Si es mayor a 4 va a imprimir, pero lo hara por
-        // unica vez.
-        //first(val => val[0] > 3),
-        // take: Podemos poner el numero de cuatas veces se va a emimit
-        // Se van a emimit 4 veces
-        //take(4),
         
-        // takeWhile: podemos hacer que se ejecute la accion mietras se cumpla x condicion
-        // Mientras que la columna sea mayor a 3, voy a estar emitiendo eventos.
-        // takeWhile( ([col, row]) => col > 3),
         
-        tap(val => console.log(`cell: [ ${val}]`)),
-        
-        // Omitir los primeros 5 eventos. despues del 5 ya emite los demas eventos.
-        skip(5)
-
+        // Espera una funcion el reduce
+        // accumulated: tiene el valor acumulado
+        // current: representa el valor actualr
+        scan( (accumulated, current) => {
+            // El acumulador me devuela en nro Click acumulados y un array con 
+            // las casillas validas clickeadas.
+            return {
+                clicks: accumulated.clicks + 1,
+                cells: [ ...accumulated.cells, current]
+            }
+        }, { clicks: 0, cells: []})
     );
-    const subscription = click$.subscribe( event => displayLog(event));
-
+    const subscription = click$.subscribe( event => 
+        displayLog(`${event.clicks} clicks: ${JSON.stringify(event.cells)}` ));
 
     /** end coding */
 }
