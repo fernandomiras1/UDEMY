6cbfd92caea7ad692434d062d3e9d09c3b2462fc
Fernando Miras <fernando.miras.pc@gmail.com>
2019-06-03 13:56:51 -0300

    delete all PWA-Fernando-Herrera

--- PWA-Fernando-Herrera/PROYECTO/02-service-worker/js/app.js
+++
@@ -1,9 +0,0 @@
-// Confirmar si podemos usar el serviceWorker
-
-if ( navigator.serviceWorker ) {
-    //console.log('el navegador soporta el sw');
-
-
-    // donde va a estar nuestro archivo de sw
-    navigator.serviceWorker.register('sw.js');
-}
--- PWA-Fernando-Herrera/PROYECTO/04-cache-offline/js/app.js
+++
@@ -1,5 +0,0 @@
-
-// Validamos si el navegador permite el SW 
-if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('sw.js');
-}
