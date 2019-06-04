770eb4a95dc6f4f639bf6ad40e8636c4204dda54
Fernando Miras <fernando.miras.pc@gmail.com>
2019-06-04 10:48:49 -0300

    delte rxjs-school-master

--- RxJS-nivel-PRO/rxjs-school-master/src/utils.js
+++
@@ -1,7 +0,0 @@
-/* Utility to show content into the view */
-export const displayLog = (content)=> {
-    let element = document.createElement('div');
-    element.innerHTML = content;
-    const logContainer = document.getElementById("log-container");
-    logContainer.appendChild(element);
-}
