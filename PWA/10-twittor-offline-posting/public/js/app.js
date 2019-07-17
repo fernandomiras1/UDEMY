67e0439791d84de5c93d925768e381d9e0de73fc
Fernando Miras <fernando.miras.pc@gmail.com>
2019-07-17 10:41:11 -0300

    PWA/10-twittor-offline-posting

--- PWA/10-twittor-offline-posting/public/js/app.js
+++ PWA/10-twittor-offline-posting/public/js/app.js
@@ -143,3 +143,19 @@ postBtn.on('click', function() {
     crearMensajeHTML( mensaje, usuario );
 
 });
+
+// obtener los mensajes de servidor
+function getMensajes() {
+    fetch('api')
+        .then( resu => resu.json())
+        .then( posts => {
+            console.log(posts);
+
+            posts.forEach(post => {
+
+                crearMensajeHTML( post.mensaje, post.user);
+            })
+        });
+}
+
+getMensajes();
--- PWA/10-twittor-offline-posting/server/routes.js
+++ PWA/10-twittor-offline-posting/server/routes.js
@@ -4,13 +4,29 @@ var router = express.Router();
 
 
 
+const mensajes = [
+  {
+    _id: 'xxx',
+    user: 'Spiderman',
+    mensaje: 'hola Mundo'
+  },
+  {
+    _id: 'xxx',
+    user: 'wolverine',
+    mensaje: 'hola wolverine'
+  },
+  {
+    _id: 'xxx',
+    user: 'ironman',
+    mensaje: 'hola ironman'
+  }
 
-
+];
 
 
 // Get mensajes
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  res.json(mensajes);
 });
 
 
