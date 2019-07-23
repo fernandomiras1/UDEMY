00954c1470946f7e31e9baf955c01262f85fffa4
Fernando Miras <fernando.miras.pc@gmail.com>
2019-07-23 10:55:57 -0300

    PWA/10-twittor-offline-posting

---
+++ PWA/.history/10-twittor-offline-posting/server/routes_20190722182932.js
@@ -0,0 +1,46 @@
+// Routes.js - Módulo de rutas
+var express = require('express');
+var router = express.Router();
+
+
+
+const mensajes = [
+  {
+    _id: 'xxx',
+    user: 'Spiderman',
+    mensaje: 'hola Mundo'
+  },
+  {
+    _id: 'xxx',
+    user: 'wolverine',
+    mensaje: 'hola wolverine'
+  },
+  {
+    _id: 'xxx',
+    user: 'ironman',
+    mensaje: 'hola ironman'
+  }
+
+];
+
+
+// Get mensajes
+router.get('/', function (req, res) {
+  res.json(mensajes);
+});
+
+// Get mensajes
+router.post('/', function (req, res) {
+
+  const mensaje = {
+    mensaje: req.b,
+    user: ''
+  };
+
+  res.json(mensajes);
+});
+
+
+
+
+module.exports = router;
