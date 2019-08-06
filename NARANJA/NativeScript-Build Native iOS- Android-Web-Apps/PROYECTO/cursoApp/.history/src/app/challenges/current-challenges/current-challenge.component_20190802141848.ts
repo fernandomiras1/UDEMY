71af6f3328f51af95656159424e3db1d52e1af0d
Fernando Miras <@fernando.miras>
2019-08-06 11:37:14 -0300

    curso de nativeScript

---
+++ NARANJA/NativeScript-Build Native iOS- Android-Web-Apps/PROYECTO/cursoApp/.history/src/app/challenges/current-challenges/current-challenge.component_20190802141848.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ns-current-challenge',
+    templateUrl: './current-challenge.component.html',
+    styleUrls: ['./current-challenge.component.scss'],
+    moduleId: module.id
+})
+export class CurrentChallengeComponent {
+
+    public challengeDescription = '';
+}
---
+++ NARANJA/NativeScript-Build Native iOS- Android-Web-Apps/PROYECTO/cursoApp/src/app/challenges/current-challenges/current-challenge.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ns-current-challenge',
+    templateUrl: './current-challenge.component.html',
+    styleUrls: ['./current-challenge.component.scss'],
+    moduleId: module.id
+})
+export class CurrentChallengeComponent {
+
+    public challengeDescription = 'tes';
+    currentChallenge = '';
+
+    onSetChallenge() {
+        this.currentChallenge = this.challengeDescription;
+    }
+}
