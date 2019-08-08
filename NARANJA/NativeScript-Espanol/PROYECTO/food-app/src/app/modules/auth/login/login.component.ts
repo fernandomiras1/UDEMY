d886f6f5e962691daa1ab91d78a62142016353d2
Fernando Miras <@fernando.miras>
2019-08-08 12:46:20 -0300

    NS-E-maquetado de login

--- NARANJA/NativeScript-Espanol/PROYECTO/food-app/src/app/modules/auth/login/login.component.ts
+++ NARANJA/NativeScript-Espanol/PROYECTO/food-app/src/app/modules/auth/login/login.component.ts
@@ -1,7 +1,13 @@
 import { Component } from "@angular/core";
+import { Page } from "tns-core-modules/ui/page/page";
 
 @Component({
     moduleId: module.id,
     templateUrl: "./login.component.html"
 })
-export class LoginComponent { }
+export class LoginComponent {
+    constructor(private page: Page) {
+        // Quitamos el accionBar ( el borde blanco del header )
+        this.page.actionBarHidden = true;
+    }
+}
