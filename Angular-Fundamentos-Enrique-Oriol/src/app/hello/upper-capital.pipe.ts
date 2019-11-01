7fa34a9aee963f8f22844054b701712c02302ec1
Fernando Miras <fernando.miras.pc@gmail.com>
2019-10-31 23:49:28 -0300

    Angular-Fundamentos-Enrique-Oriol-Agrego Comentarios para entender mejor

--- Angular-Fundamentos-Enrique-Oriol/src/app/hello/upper-capital.pipe.ts
+++ Angular-Fundamentos-Enrique-Oriol/src/app/hello/upper-capital.pipe.ts
@@ -3,8 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
     name: 'upperCapital'
 })
-export class UpperCapitalPipe implements PipeTransform{
-    transform(value:string){
+export class UpperCapitalPipe implements PipeTransform {
+    transform(value: string) {
+      //  la primera letra en mayuscula y todas las demas en minuscula
         return value[0].toUpperCase() + value.substring(1).toLowerCase();
     }
 }
