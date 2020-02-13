f86e2344156af6e68094cf0ab1c045ae26bfb6c6
Fernando Miras <@fernando.miras>
2020-02-13 17:36:01 -0300

    migracion angular 9

--- Angular-Fernando-Herrera/21. Virtual Scroll y Drag-Drop nuevo en  Angular 7/virtualScroll/src/app/app.module.ts
+++ Angular-Fernando-Herrera/21. Virtual Scroll y Drag-Drop nuevo en  Angular 7/virtualScroll/src/app/app.module.ts
@@ -6,29 +6,23 @@ import { VirtualComponent } from './virtual/virtual.component';
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 
 import { DragComponent } from './drag/drag.component';
 import { PaisesComponent } from './paises/paises.component';
-import { from } from 'rxjs';
-import { GraficosTestComponent } from './graficos-test/graficos-test.component';
-import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
     VirtualComponent,
     DragComponent,
-    PaisesComponent,
-    GraficosTestComponent
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule,
-    HttpClientModule,
-    ChartsModule
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
