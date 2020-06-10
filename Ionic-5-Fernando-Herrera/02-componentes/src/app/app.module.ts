43425bf7773c585126eeb2bd62c97f43ff9ba85e
Fernando Miras <fernando.miras.pc@gmail.com>
2020-06-10 14:21:50 -0300

    feat: basic ionic componentes

--- Ionic-5-Fernando-Herrera/02-componentes/src/app/app.module.ts
+++ Ionic-5-Fernando-Herrera/02-componentes/src/app/app.module.ts
@@ -8,11 +8,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ Ionic-5-Fernando-Herrera/02-componentes/src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'archive',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
