fd98d8bd7b7b35b45d51478eb3a80f9c1b94a71e
Fernando Miras <fernando.miras.pc@gmail.com>
2020-06-10 17:51:29 -0300

    ionic

--- Ionic-5-Fernando-Herrera/02-componentes/src/app/pages/action-sheet/action-sheet.page.ts
+++ Ionic-5-Fernando-Herrera/02-componentes/src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+  constructor(public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
   }
