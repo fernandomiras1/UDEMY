3901e8ce77d64065f91b222bd1374395ec4cdb0e
fernandomiras1 <ernando.miras.pc@gmail.com>
2020-06-12 14:46:53 -0300

    update new proyecto ingreso-egreso-redux

---
+++ Angular-REDUX-NGRX-Fernando-Herrera/PROYECTO/ingreso-egreso-app-new/README.md
@@ -0,0 +1,9 @@
+# IngresoEgreso
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.2.
+
+## Login
+
+ingresar al sitio: 
+user: fernando.miras.pc@gmail.com
+pass: fer123
---
+++ Angular-REDUX-NGRX-Fernando-Herrera/PROYECTO/ingreso-egreso-app-new/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDmAX8qbZGdF1EMTo6r4v7L126JcxSocnE',
+    authDomain: 'ingreso-egreso-app-8dec0.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-8dec0.firebaseio.com',
+    projectId: 'ingreso-egreso-app-8dec0',
+    storageBucket: 'ingreso-egreso-app-8dec0.appspot.com',
+    messagingSenderId: '902448700150'
+  }
+};
