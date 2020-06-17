32933fa90367ff2105274aac612c7e40445ab6a6
Fernando Miras <fernando.miras.pc@gmail.com>
2020-06-16 23:54:53 -0300

    feat: instalo zumo

--- NARANJA/app-test-zumo-blank/src/app/app.module.ts
+++ NARANJA/app-test-zumo-blank/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NGZButtonModule } from 'zumo';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NGZButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
