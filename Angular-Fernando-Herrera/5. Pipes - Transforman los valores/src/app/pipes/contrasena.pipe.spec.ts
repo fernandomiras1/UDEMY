0a9c4285bf946d5f201095fb92e992570ccb7198
Fernando Miras <fernando.miras.pc@gmail.com>
2020-06-30 09:46:34 -0300

    borro los spec

--- Angular-Fernando-Herrera/5. Pipes - Transforman los valores/src/app/pipes/contrasena.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ContrasenaPipe } from './contrasena.pipe';
-
-describe('ContrasenaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContrasenaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
