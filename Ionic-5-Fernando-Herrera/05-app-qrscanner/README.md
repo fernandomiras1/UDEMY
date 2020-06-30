18e43aea1de4c825a0b65f56d6d219eaf8d1b6f4
Fernando Miras <fernando.miras.pc@gmail.com>
2020-06-30 15:24:21 -0300

    ionic scanqr avances

---
+++ Ionic-5-Fernando-Herrera/05-app-qrscanner/README.md
@@ -0,0 +1,6 @@
+generar codigo QR
+
+https://www.qrcode.es/es/generador-qr-code/
+
+plugin
+https://ionicframework.com/docs/native/barcode-scanner
--- Ionic-5-Fernando-Herrera/05-app-qrscanner/src/app/pages/tab1/tab1.page.ts
+++ Ionic-5-Fernando-Herrera/05-app-qrscanner/src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  slidesOptions = {
+    allowSlidePrev: false,
+    allowSlideNext: false,
+  };
+
+  constructor(private barcodeScanner: BarcodeScanner) {}
+  // cilcos de vida de ionic
+  // https://ionicframework.com/docs/v3/api/navigation/NavController/#viewDidEnter
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  // es como el ngOnInit. cada vez que se inicialice el componente
+  ionViewDidEnter(){
+   console.log('ionViewDidEnter');
+  }
+
+  scan() {
+
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
+
+  // cuando salis del componente
+  ionViewDidLeave(){
+   console.log('ionViewDidLeave');
+  }
 
 }
