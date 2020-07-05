eabb0654821d90b17d2b9176709f5ff0061af43e
Fernando Miras <fernando.miras.pc@gmail.com>
2020-07-05 15:29:54 -0300

    juego de poker terminado

--- JavaScript-Moderno-Fernando-Herrera/02-blackjack/assets/js/juego.js
+++ JavaScript-Moderno-Fernando-Herrera/02-blackjack/assets/js/juego.js
@@ -1,13 +1,13 @@
 /**
- * 2C = Two of Clubs
- * 2D = Two of Diamonds
- * 2H = Two of Hearts
- * 2S = Two of Spades
+ * 2C = Two of Clubs (2 de treboles)
+ * 2D = Two of Diamonds (2 de Diamante)
+ * 2H = Two of Hearts (2 de Corazones)
+ * 2S = Two of Spades (2 de Espada)
  */
 
-let deck         = [];
-const tipos      = ['C','D','H','S'];
-const especiales = ['A','J','Q','K'];
+let deck         = []; // baraja de cartas
+const tipos      = ['C','D','H','S']; // tipos de cartas
+const especiales = ['A','J','Q','K']; // el valor del juego es 10 puntos, menos es 'A' q vale 11
 
 let puntosJugador = 0,
     puntosComputadora = 0;
@@ -22,22 +22,23 @@ const divCartasComputadora = document.querySelector('#computadora-cartas');
 
 const puntosHTML = document.querySelectorAll('small');
 
-// Esta función crea un nuevo deck
+// Esta función crea una nueva baraja
 const crearDeck = () => {
 
+    // Recoremos los numeros
     for( let i = 2; i <= 10; i++ ) {
         for( let tipo of tipos ) {
             deck.push( i + tipo);
         }
     }
-
+    // recoremos las letras
     for( let tipo of tipos ) {
         for( let esp of especiales ) {
             deck.push( esp + tipo);
         }
     }
     // console.log( deck );
-    deck = _.shuffle( deck );
+    deck = _.shuffle( deck ); // devuele un array alatorio
     console.log( deck );
     return deck;
 }
@@ -49,16 +50,23 @@ crearDeck();
 const pedirCarta = () => {
 
     if ( deck.length === 0 ) {
-        throw 'No hay cartas en el deck';
+        throw 'No hay cartas en la baraja';
     }
-    const carta = deck.pop();
+    const carta = deck.pop(); // tomo la ultima carta
     return carta;
 }
 
-// pedirCarta();
+// Necesitamos saber cual es el valor de esa carta
 const valorCarta = ( carta ) => {
-
-    const valor = carta.substring(0, carta.length - 1);
+    // substring: voy a cortar de la posicion 0 y oviar la ultima letra
+    const valor = carta.substring(0, carta.length - 1); // extraemos la primera letra o numero
+    // isNaN = si es un numero, un true si no es un numero
+    if (isNaN( valor )) {
+        console.log('No es un numero');
+    } else {
+        console.log('Es un numero');
+    }
+    // valor * 1 = lo pasamos a un numero. porq esta en string
     return ( isNaN( valor ) ) ? 
             ( valor === 'A' ) ? 11 : 10
             : valor * 1;
@@ -79,12 +87,14 @@ const turnoComputadora = ( puntosMinimos ) => {
         imgCarta.classList.add('carta');
         divCartasComputadora.append( imgCarta );
 
+        // el jugador perdio. Calquier carta que la computadora saque gana
         if( puntosMinimos > 21 ) {
             break;
         }
 
     } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
 
+    // ejecuta esto depues que termine de correr el hilo de do while
     setTimeout(() => {
         if( puntosComputadora === puntosMinimos ) {
             alert('Nadie gana :(');
@@ -138,7 +148,7 @@ btnDetener.addEventListener('click', () => {
 });
 
 btnNuevo.addEventListener('click', () => {
-
+    // resetea el juego
     console.clear();
     deck = [];
     deck = crearDeck();
