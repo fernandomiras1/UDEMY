1bf27f2659fa8626d7214db27098f4b2793e6f31
Fernando Miras <fernando.miras.pc@gmail.com>
2020-07-09 21:26:23 -0300

    objetos javascript

--- JavaScript-Moderno-Fernando-Herrera/04-todo/src/classes/todo-list.class.js
+++ JavaScript-Moderno-Fernando-Herrera/04-todo/src/classes/todo-list.class.js
@@ -1,20 +1,24 @@
 export class TodoList {
     constructor() {
         this.todos = [];
+        this.cargarLocalStorage();
     }
 
     nuevoTodo( todo ) {
         this.todos.push(todo);
+        this.guardarLocalStorage();
     }
 
     borrarTodo(id) {
         this.todos = this.todos.filter(todo => todo.id != id);
+        this.guardarLocalStorage();
     }
 
     marcarCompletado(id) {
         for (const todo of this.todos) {
             if ( todo.id === id ) {
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
@@ -23,5 +27,16 @@ export class TodoList {
     borrarCompletado() {
         // necesito todos lo que no esten completado ( false )
         this.todos = this.todos.filter(todo => !todo.completado);
+        this.guardarLocalStorage();
+    }
+
+    guardarLocalStorage() {
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+    }
+
+    cargarLocalStorage() {
+        this.todos = (localStorage.getItem('todo')) ? 
+            JSON.parse(localStorage.getItem('todo')) :
+            [];
     }
 }
