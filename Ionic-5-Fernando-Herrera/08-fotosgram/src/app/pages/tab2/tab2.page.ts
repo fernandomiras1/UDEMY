cfb47cd0b5b79893be5dadfa9e285ad108c4b3b5
fernandomiras1 <fernando.miras.pc@gmail.com>
2020-09-15 12:15:52 -0300

    feat: android capacitor

--- Ionic-5-Fernando-Herrera/08-fotosgram/src/app/pages/tab2/tab2.page.ts
+++ Ionic-5-Fernando-Herrera/08-fotosgram/src/app/pages/tab2/tab2.page.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { PostsService } from '../../services/posts.service';
 import { Router } from '@angular/router';
-import { Plugins } from '@capacitor/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { AnyTxtRecord } from 'dns';
 
-const { Geolocation } = Plugins;
+const { Geolocation, Camera } = Plugins;
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -11,6 +14,8 @@ const { Geolocation } = Plugins;
 })
 export class Tab2Page {
 
+  imgage: SafeResourceUrl;
+  imaggg: any;
   tempImages: string[] = [];
   cargandoGeo = false;
   post = {
@@ -19,7 +24,10 @@ export class Tab2Page {
     posicion: false
   };
 
+  @ViewChild('imageElement') imageElement: any;
+
   constructor(private postsService: PostsService,
+    private domSanitizer: DomSanitizer,
               private route: Router) {}
 
 
@@ -53,6 +61,26 @@ export class Tab2Page {
 
   getCurrentPosition() {
     return Geolocation.getCurrentPosition();
+  }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 75,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+      correctOrientation: true
+    });
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    var imageUrl = image.webPath;
+    this.imaggg = image.webPath;
+    console.log(imageUrl);
+    // Can be set to the src of an image now
+    // console.log(image.base64String);
+    this.imgage = this.domSanitizer.bypassSecurityTrustResourceUrl(image && image.dataUrl);
   }
 
 }
