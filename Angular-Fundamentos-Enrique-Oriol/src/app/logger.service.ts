47c7f3bd0a6d1c9c056260eaedc562495ec9af54
fernandomiras1 <fernando.miras.pc@gmail.com>
2020-12-12 11:55:46 -0300

    fix: validate

--- Angular-Fundamentos-Enrique-Oriol/src/app/logger.service.ts
+++ Angular-Fundamentos-Enrique-Oriol/src/app/logger.service.ts
@@ -3,8 +3,10 @@ import { Injectable } from "@angular/core";
 @Injectable({
     providedIn: 'root'
 })
+
 export class LoggerService{
-    log(msg: any)   { console.log(msg); }
+  prefix =  'root';
+  log(msg: any)   { console.log(this.prefix + ': ' + msg); }
     error(msg: any) { console.error(msg); }
     warn(msg: any)  { console.warn(msg); }
 }
