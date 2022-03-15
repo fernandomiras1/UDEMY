20e61e9f27bafe023c50ba9021cd2ed039888a9a
Fernando Miras <fernando.miras.pc@gmail.com>
2022-03-15 18:49:32 -0300

    add PrintJsonObjectModule

--- CSS-Grid-Flexbox/project-angular-css-all/src/app/components/base-print-json-object/base-print-json-object.component.ts
+++ CSS-Grid-Flexbox/project-angular-css-all/src/app/components/base-print-json-object/base-print-json-object.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'st-base-print-json-object',
-  styleUrls: ['./base-print-json-object.component.scss'],
-  template: '<pre><code [innerHTML]="formatted(json)"></code></pre>',
+  selector: "st-base-print-json-object",
+  styleUrls: ["./base-print-json-object.component.scss"],
+  template: '<pre><code [innerHTML]="formatted(json) | html"></code></pre>',
   encapsulation: ViewEncapsulation.None,
 })
 export class BasePrintJsonObjectComponent {
@@ -30,17 +30,17 @@ export class BasePrintJsonObjectComponent {
       let key = '<span class="json-key">',
         val = '<span class="json-value">',
         str = '<span class="json-string">',
-        r = pIndent || '';
-      if (pKey) r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
-      if (pVal) r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
-      return r + (pEnd || '');
+        r = pIndent || "";
+      if (pKey) r = r + key + pKey.replace(/[": ]/g, "") + "</span>: ";
+      if (pVal) r = r + (pVal[0] == '"' ? str : val) + pVal + "</span>";
+      return r + (pEnd || "");
     };
 
     return JSON.stringify(data, null, 2)
-      .replace(/&/g, '&amp;')
-      .replace(/\\"/g, '&quot;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
+      .replace(/&/g, "&amp;")
+      .replace(/\\"/g, "&quot;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
       .replace(jsonLine, replacer);
   }
 }
