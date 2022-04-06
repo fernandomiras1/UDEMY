2c3117e2df345d59b15c2e913fb45aa8a9ba0631
Fernando Miras <fernando.miras.pc@gmail.com>
2022-04-06 00:55:35 -0300

    done

--- CSS-Grid-Flexbox/project-angular-css-all/src/app/pages/rich-text-editor-page/rich-text-editor-page.component.ts
+++ CSS-Grid-Flexbox/project-angular-css-all/src/app/pages/rich-text-editor-page/rich-text-editor-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import { groupBy, forEach } from 'lodash-es';
 @Component({
   selector: 'app-rich-text-editor-page',
   templateUrl: './rich-text-editor-page.component.html',
@@ -8,8 +7,6 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   encapsulation: ViewEncapsulation.None,
 })
 export class RichTextEditorPageComponent implements OnInit {
-  public Editor = ClassicEditor;
-
   testOriginal =
     '<ol><li>Level_1</li><li>Level_1</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-2">Level_3</li><li class="ql-indent-3">Level_4</li></ol>';
 
@@ -22,10 +19,7 @@ export class RichTextEditorPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const test2 =
-      '<ol><li>Level_1</li><li>Level_1</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-2">Level_3</li><li class="ql-indent-3">Level_4</li></ol>';
-
-    this.fnHTML(test2);
+    this.fnHTML();
     this.content = this.testOriginal;
     this.modules = {
       toolbar: [
@@ -71,17 +65,114 @@ export class RichTextEditorPageComponent implements OnInit {
     console.log('content2', this.content2);
   }
 
-  fnHTML(body: string) {
+  fnHTML() {
+    const body =
+      '<ol><li>Level_1</li><li>Level_1</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-2">Level_3</li><li class="ql-indent-3">Level_4</li></ol>';
+
     const doc = new DOMParser().parseFromString(body, 'text/xml');
 
+    console.log('antes doc', new DOMParser().parseFromString(body, 'text/xml'));
     const indents = doc.querySelectorAll('[class*="ql-indent-"]');
+    const newOL = document.createElement('ol');
 
-    indents.forEach(i => {
-      const newOL = document.createElement('ol');
+    const indentsNew = [];
 
-      i.parentElement.append(newOL);
-    });
+    this.groupNodes(indents);
 
     console.log('doc', doc);
+
+    const allOls = doc.querySelectorAll('ol');
+
+    allOls.forEach(x => {
+      if (x.textContent.length <= 1) {
+        x.parentNode.removeChild(x);
+      }
+    });
+    // const array = [];
+
+    // indents.forEach((i, index: number) => {
+    //   // i.removeAttribute('class');
+    //   console.log('i.className', i.className);
+    //   if (i.className == `ql-indent-${index}`) {
+    //     // console.log('i.className', i.className, index);
+    //     // i.removeAttribute('class');
+    //     array.push(i);
+    //   }
+    //   // i.innerHTML = '<ol><li>' + i.innerHTML + '</li></ol>';
+    //   console.log('i', array);
+    //   // indentsNew.push('<ol><li>' + i.innerHTML + '</li></ol>');
+    // });
+    // ul.setAttribute('id','proList');
+
+    // productList = ['Electronics Watch','House wear Items','Kids wear','Women Fashion'];
+
+    // doc.querySelector('.ql-indent-1').appendChild(ol);
+    // console.log('object', doc.querySelector('.ql-indent-1'));
+    // indents.forEach(this.renderProductList);
+  }
+
+  groupNodes(listI) {
+    const list: any[] = [].slice.call(listI);
+    const parent = list[0].parentElement;
+    const groupTest = groupBy(list, 'className');
+
+    const forEachTest = forEach(groupTest, (arr, coin) => {
+      console.log('arr', arr);
+      // const parent = arr[0].parentElement;
+
+      arr.forEach((item, index) => {
+        const lastWrapper = document.createElement('ol');
+
+        item.removeAttribute('class');
+        parent.appendChild(lastWrapper);
+
+        [].forEach.call(arr.slice(0, arr.length), function (x) {
+          lastWrapper.appendChild(x);
+        });
+      });
+      console.log('coin', coin);
+    });
+
+    // console.log('list', list);
+    console.log('forEachTestforEachTest', forEachTest);
+
+    // for (let i = 0; i < list.length; i += groupByT) {
+    //   const lastWrapper = document.createElement('ol');
+
+    //   parent.appendChild(lastWrapper);
+
+    //   [].forEach.call(list.slice(i, i + groupByT), function (x) {
+    //     lastWrapper.appendChild(x);
+    //   });
+    // }
+  }
+
+  renderProductList(element, index, arr) {
+    // const li = document.createElement('li');
+    const ol = document.createElement('ol');
+
+    // li.setAttribute('class', 'item');
+    element.parentNode.append(ol);
+
+    // ul.appendChild(li);
+
+    // li.innerHTML = li.innerHTML + element;
+  }
+
+  renderOL(body: string) {
+    const doc = new DOMParser().parseFromString(body, 'text/xml');
+    const indents = doc.querySelectorAll('[class*="ql-indent-"]');
+    const a = '<ol>',
+      b = '</ol>',
+      m = [];
+
+    // Right now, this loop only works with one
+    // explicitly specified array (options[0] aka 'set0')
+    for (let i = 0; i < indents.length; i += 1) {
+      m[i] = '<li>' + indents[i] + '</li>';
+    }
+
+    // document.getElementById('foo').innerHTML = a + m + b;
+    console.log(a + m + b);
   }
 }
