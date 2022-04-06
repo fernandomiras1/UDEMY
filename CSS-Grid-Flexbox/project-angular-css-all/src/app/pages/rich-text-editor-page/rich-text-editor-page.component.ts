3048266e6ab120bc65b963d88e4e5252e0e0bded
Fernando Miras <fernando.miras.pc@gmail.com>
2022-04-05 21:22:57 -0300

    app-infinity-css-page

--- CSS-Grid-Flexbox/project-angular-css-all/src/app/pages/rich-text-editor-page/rich-text-editor-page.component.ts
+++ CSS-Grid-Flexbox/project-angular-css-all/src/app/pages/rich-text-editor-page/rich-text-editor-page.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-rich-text-editor-page',
   templateUrl: './rich-text-editor-page.component.html',
   styleUrls: ['./rich-text-editor-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class RichTextEditorPageComponent implements OnInit {
+  public Editor = ClassicEditor;
+
+  testOriginal =
+    '<ol><li>Level_1</li><li>Level_1</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-2">Level_3</li><li class="ql-indent-3">Level_4</li></ol>';
+
+  test =
+    '<div class="zd-comment" dir="auto">Test with multi layer list:<br><ol dir="auto"><li>One</li><li>Two<ol dir="auto"><li>Two.One</li><li>Two.Two<ol dir="auto"><li>Two.Two.One</li></ol></li></ol></li><li>Three</li></ol></div>';
+
   content = '';
   content2 = '';
   modules = {};
   constructor() {}
 
   ngOnInit() {
+    const test2 =
+      '<ol><li>Level_1</li><li>Level_1</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-1">Level_2</li><li class="ql-indent-2">Level_3</li><li class="ql-indent-3">Level_4</li></ol>';
+
+    this.fnHTML(test2);
+    this.content = this.testOriginal;
     this.modules = {
       toolbar: [
         [
@@ -54,5 +69,19 @@ export class RichTextEditorPageComponent implements OnInit {
   onClick() {
     console.log('content', this.content);
     console.log('content2', this.content2);
+  }
+
+  fnHTML(body: string) {
+    const doc = new DOMParser().parseFromString(body, 'text/xml');
+
+    const indents = doc.querySelectorAll('[class*="ql-indent-"]');
+
+    indents.forEach(i => {
+      const newOL = document.createElement('ol');
+
+      i.parentElement.append(newOL);
+    });
+
+    console.log('doc', doc);
   }
 }
